24fc991e7f4d2dee7d3dbbbccfffd66294c061fa
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-24 23:59:39 +0000

    array created to store the ids of clicked images and callback function added to click events

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,32 @@ import Intro from './components/intro';
 import LandingPage from './components/landingpage.index'
 import pictures from "../pictures.json/";
 import Footer from './components/footer'
-
+ let checkArray = [];
 function App() {
+  // usestate to shuffle that pictures array
   const [picturedisplay, setPictures] = useState(pictures)
-
+  
+  // function to shuffle array and set new array into state
   const shufflePictures = () => {
     const newArray = [...picturedisplay]
      newArray.sort(() => Math.random() - 0.5); 
     console.log(newArray)
-    // return newArray;
-    setPictures(newArray)
+    setPictures(newArray) 
+   
   }; 
+
+
+
+  // usestate to increment score and display success message
+
+  const [score, setScore] = useState(0);
+
+  // function to check if the picture id has been clicked already
+
+  const updateScore = (id) => {
+   checkArray.push(id);
+   console.log(checkArray)
+  }
 
   
 
@@ -30,7 +45,9 @@ function App() {
       key = {picture.id}
       id = {picture.id}
       image = {picture.image}
-      shufflePictures = {shufflePictures}/>
+      shufflePictures = {shufflePictures}
+      updateScore = {updateScore}
+      />
     ))}
     <Footer />
     
--- src/components/landingpage.index.jsx
+++ src/components/landingpage.index.jsx
@@ -4,19 +4,20 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Container"
 import pictures from "../../pictures.json";
 import App from "../App";
+// let checkArray = [];
+const LandingPage = ({id, image, shufflePictures, updateScore}) => {
 
-const LandingPage = ({id, image, shufflePictures}) => {
+  const handleClick = (id) => { 
+    console.log(id);
+    shufflePictures();
 
-  return (
-   
-    
-    
-      <Card.Img variant="top" src={image} onClick={shufflePictures}className="img-container" style={{ width: '17.5rem', height : '16rem', margin : "1rem"}}/>
-   
-      
-   
-    
-  );
+    // checkArray.push(id);
+    // console.log(checkArray)
+   }
+  
+   return (
+    <Card.Img variant="top" src={image} onClick={() => {{handleClick(id), updateScore(id)}}} className="img-container" style={{ width: '17.5rem', height : '16rem', margin : "1rem"}}/>
+      );
 }
 
 
