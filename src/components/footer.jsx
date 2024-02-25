cd84599d7837009cd127c5e0cb32d6b16727c8d1
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-25 16:23:23 +0000

    name on footer changed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="container-fluid d-flex footer justify-content-between" style={{ width: "100%", height: "20%", alignItems: "center" }}> 
     <div>Guess-Click-Game </div> 
     <div><a style= {{color : "#23001E"}}  target= "_blank" href="https://github.com/chrisma89"><FontAwesomeIcon icon={faGithub} /></a></div>    
-  <div>©Chrisma Caroline Stephen</div>
+  <div>©Chrisma Stephen</div>
  
  
 </div>
