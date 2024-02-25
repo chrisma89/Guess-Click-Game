d5dad7ff03b2324f29520c7142a39d6df98d4273
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-25 16:22:08 +0000

    footer styling changed and github link added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faGithub } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
   return ( 
-    <div className="container-fluid d-flex footer" style = {{width : "100%", height : "20%"}}>
-       ©Chrisma Caroline Stephen | Guess-Click-Game
-    </div>
+    <div className="container-fluid d-flex footer justify-content-between" style={{ width: "100%", height: "20%", alignItems: "center" }}> 
+    <div>Guess-Click-Game </div> 
+    <div><a style= {{color : "#23001E"}}  target= "_blank" href="https://github.com/chrisma89"><FontAwesomeIcon icon={faGithub} /></a></div>    
+  <div>©Chrisma Caroline Stephen</div>
+ 
+ 
+</div>
+
+    
    
     
    );
